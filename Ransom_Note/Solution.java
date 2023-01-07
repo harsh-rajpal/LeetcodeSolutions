c89738abb954ca4f2ac57bba027db2abcc170ccf
harsh-rajpal <harshrajpal2k18@gmail.com>
2023-01-07 23:01:58 +0530

    Updated

--- Ransom_Note/Solution.java
+++ Ransom_Note/Solution.java
@@ -1,21 +1,4 @@
 package Ransom_Note;
-
-// Given two strings ransomNote and magazine,return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
-
-// Each letter in magazine can only be used once in ransomNote.
-
-// Example 1:
-
-// Input:ransomNote="a",magazine="b"Output:false Example 2:
-
-// Input:ransomNote="aa",magazine="ab"Output:false Example 3:
-
-// Input:ransomNote="aa",magazine="aab"Output:true
-
-// Constraints:
-
-// 1<=ransomNote.length,magazine.length<=105 ransomNote and magazine consist of lowercase English letters.
-
 public class Solution {
 
     public boolean canConstruct(String ransomNote, String magazine) {
@@ -31,11 +14,11 @@ public class Solution {
         return true;
     }
     //Algorithm
-    1. Create an array of size 26 to store the frequency of each character in magazine.
-    2. Traverse through the magazine string and increment the frequency of each character in the array.
-    3. Traverse through the ransomNote string and decrement the frequency of each character in the array.
-    4. If the frequency of any character in the array becomes negative, return false.
-    5. Return true.
+    // 1. Create an array of size 26 to store the frequency of each character in magazine.
+    // 2. Traverse through the magazine string and increment the frequency of each character in the array.
+    // 3. Traverse through the ransomNote string and decrement the frequency of each character in the array.
+    // 4. If the frequency of any character in the array becomes negative, return false.
+    // 5. Return true.
 
     //Time Complexity: O(n+m) where n is the length of ransomNote and m is the length of magazine.
 
