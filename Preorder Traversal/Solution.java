01a981f295e66f1e174c3ba732034083f6773d52
harsh-rajpal <harshrajpal2k18@gmail.com>
2023-01-09 11:44:12 +0530

    POD

---
+++ Preorder Traversal/Solution.java
@@ -0,0 +1,48 @@
+package Preorder Traversal;
+
+import java.util.*;
+
+public class TreeNode {
+    
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+}
+public class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        
+        if (root == null) {
+            return result;
+        }
+        
+        stack.push(root);
+        while (!stack.empty()) {
+            TreeNode node = stack.pop();
+            result.add(node.val);
+            
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        
+        return result;
+    }
+}
