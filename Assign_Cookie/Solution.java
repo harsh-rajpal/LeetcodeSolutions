7b7dab8bdd59955dd20dd37e9a2d77612a902e95
harsh-rajpal <harshrajpal2k18@gmail.com>
2023-01-14 23:47:22 +0530

    Pratice

---
+++ Assign_Cookie/Solution.java
@@ -0,0 +1,20 @@
+package Assign_Cookie;
+
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int numberOfChildren = g.length;
+        int numberOfCookies = s.length;
+        int cookie = 0, answer = 0;
+        for (int i = 0; i < numberOfChildren && cookie < numberOfCookies;) {
+            if (s[cookie] >= g[i]) {
+                i++;
+                answer++;
+            }
+            cookie++;
+        }
+        return answer;
+
+    }
+}
